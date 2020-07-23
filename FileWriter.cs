baa685626e526c94f653247235fd62de123b1c51
stefanfeurle <stefan.feurle@yahoo.com>
2020-07-23 14:03:38 +0200

    further commit

---
+++ FileWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var dir = Directory.GetFiles(@"C:\Users\DCV\Downloads");
+            var fileNames = "";
+            foreach(var item in dir)
+            {
+                fileNames = string.IsNullOrEmpty(fileNames) ? item : fileNames + "\n" + item;
+            }
+
+            System.IO.File.WriteAllText(@"C:\Users\DCV\Downloads\myFiles.txt", fileNames);
+        }
+    }
+}
